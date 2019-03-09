4a95d6099b015527f3d84477fe52bcde3ad4d326
Marcus Thuvesen <marcus.thuvesen@gmail.com>
2019-03-09 19:21:13 +0100

    Initial Commit

---
+++ pagetest/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  pagetest
+//
+//  Created by Marcus thuvesen on 2019-03-09.
+//  Copyright © 2019 Marcus thuvesen. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
